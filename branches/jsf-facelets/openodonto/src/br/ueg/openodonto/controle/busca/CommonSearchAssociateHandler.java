20cf88f8f7fa6aff708b02a77cb174476aaa4008
amanda.barbosabs <amanda.barbosabs@272ffaea-26e4-11df-be0e-258e15093178>
2010-09-28 01:05:38 +0000

    Finalização Parcial da implementação para Iteração n 3

--- branches/jsf-facelets/openodonto/src/br/ueg/openodonto/controle/busca/CommonSearchAssociateHandler.java
+++ branches/jsf-facelets/openodonto/src/br/ueg/openodonto/controle/busca/CommonSearchAssociateHandler.java
@@ -1,13 +1,54 @@
 package br.ueg.openodonto.controle.busca;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import br.ueg.openodonto.persistencia.EntityManager;
+import br.ueg.openodonto.persistencia.dao.DaoCrud;
+import br.ueg.openodonto.persistencia.orm.Entity;
+import br.ueg.openodonto.persistencia.orm.OrmFormat;
+import br.ueg.openodonto.servico.busca.ResultFacade;
 import br.ueg.openodonto.servico.busca.event.AbstractSearchListener;
 import br.ueg.openodonto.servico.busca.event.SearchAssociateEvent;
 
-public class CommonSearchAssociateHandler extends AbstractSearchListener{
+public abstract class CommonSearchAssociateHandler<T extends Entity> extends AbstractSearchListener{
 
+	private EntityManager<T> dao;
+	
+	public CommonSearchAssociateHandler(EntityManager<T> dao) {
+		this.dao = dao;
+	}	
+	
 	@Override
 	public void associatePerformed(SearchAssociateEvent event) {
-		System.out.println(event.getSelecteds());
+		List<T> associated = new ArrayList<T>();
+		for(ResultFacade result : event.getSelecteds()){
+			try{
+				T entity = ((DaoCrud<T>)dao).getNewEntity();			
+				OrmFormat format = new OrmFormat(entity);
+				format.parse(result.getValue());
+				load(entity);
+				associated.add(entity);
+			}catch (Exception e) {
+				e.printStackTrace();
+			}		
+		}
+		associateBeans(associated);
 	}
+	
+	public abstract void associateBeans(List<T> associated);
+	
+	public void load(T bean){
+		try {
+			dao.load(bean);
+			extraLoad();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void extraLoad(){		
+	}
+	
 	
 }
--- branches/jsf-facelets/openodonto/src/br/ueg/openodonto/dominio/Produto.java
+++ branches/jsf-facelets/openodonto/src/br/ueg/openodonto/dominio/Produto.java
@@ -13,6 +13,7 @@ import br.ueg.openodonto.persistencia.orm.Relationship;
 import br.ueg.openodonto.persistencia.orm.Table;
 import br.ueg.openodonto.persistencia.orm.value.EnumValue;
 import br.ueg.openodonto.persistencia.orm.value.IdIncrementType;
+import br.ueg.openodonto.util.WordFormatter;
 
 @Table(name = "produtos")
 public class Produto implements Entity,Serializable{
@@ -71,6 +72,10 @@ public class Produto implements Entity,Serializable{
 
 	public String getDescricao() {
 		return descricao;
+	}
+	
+	public String getDescricaoResumida(){
+		return WordFormatter.abstractStr(getDescricao(), 30);
 	}
 
 	public void setDescricao(String descricao) {
