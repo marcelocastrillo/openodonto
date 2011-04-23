ea8da70fe80c196b719724ebc4f437ac9b999802
viiniiciius <viiniiciius@272ffaea-26e4-11df-be0e-258e15093178>
2011-04-23 21:15:00 +0000

    Extração do componente de validação.

--- trunk/src/openodonto/src/br/ueg/openodonto/validator/tipo/DateValidatorType.java
+++
@@ -1,7 +0,0 @@
-package br.ueg.openodonto.validator.tipo;
-
-import java.util.Date;
-
-public interface DateValidatorType extends ObjectValidatorType{
-	Date getValue();
-}
--- trunk/src/openodonto/src/br/ueg/openodonto/validator/tipo/StringValidatorType.java
+++
@@ -1,5 +0,0 @@
-package br.ueg.openodonto.validator.tipo;
-
-public interface StringValidatorType extends ObjectValidatorType{
-	String getValue();
-}
